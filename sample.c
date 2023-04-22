6b3bd7df42e7b62a6b9d1394e69abea8f2721a87
Aritra Datta <87863471+arimax32@users.noreply.github.com>
2023-04-22 13:47:19 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void func2(int s, float y){
+	return (s+y)*3;
+}
+
+int main(){
+	int y = 4,x=5;
+	float z;
+	read z;
+	print x+z;
+
+	if(x < z){
+		print z;
+	}
+	else{
+		print x;
+	}
+	int res = func2(y,z);
+	print res;
+	return 0;
+}
